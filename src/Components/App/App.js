b44da2b32fc02736375d681f19902ddac1155a3f
Josh Aragon <josharagon99@gmail.com>
2021-04-10 11:19:35 -0600

    Add basic Nav component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Nav from '../Nav/Nav'
 import './App.css';
 
 class App extends Component {
@@ -8,9 +9,9 @@ class App extends Component {
 
   render() {
     return (
-      <section>
-        <h1>here</h1>
-      </section>
+      <main>
+        <Nav />
+      </main>
     )
   }
 }
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Nav.css'
+
+const Nav = () => {
+    return (
+        <nav>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+        </nav>
+    )
+}
+
+export default Nav;

4229ca7127d46fe48abea25d4346d8b49f7774a0
Josh Aragon <josharagon99@gmail.com>
2021-04-10 11:02:50 -0600

    Build file structure

---
+++ src/Components/App/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <section>
+        <h1>here</h1>
+      </section>
+    )
+  }
+}
+
+export default App;

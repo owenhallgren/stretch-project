75d6c233b02d1c2b580fe657a6c74a2b54d38511
Josh Aragon <josharagon99@gmail.com>
2021-04-14 14:26:17 -0600

    no result issue

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -7,7 +7,7 @@ const Form = ({sortByLanguage}) => {
       <form>
         <label htmlFor="languageFilter">Sort by: </label>
         <select onChange={(e) => sortByLanguage(e.target.value)} id="languageFilter">
-          <option value="all" defaultValue></option>
+          <option value="" defaultValue></option>
           <option value="C">C</option>
           <option value="C+">C+</option>
           <option value="C++">C++</option>

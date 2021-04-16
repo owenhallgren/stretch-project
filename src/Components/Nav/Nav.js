27dc4d13d0e752da97f88faafb63d6c1d805a275
Josh Aragon <josharagon99@gmail.com>
2021-04-15 19:01:00 -0600

    add app logo and name to nav bar

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import toDo from '../../icons/to-do.png'
+import pear from '../../icons/pear.png'
 import './Nav.css'
 
 const Nav = ({resetFilteredReviews}) => {
     return (
         <nav>
+            <div className='app-logo'>
+              <img src={pear} alt='pear-logo' className='pear-logo'></img>
+              <h1>Pear</h1>
+            </div>
             <Route exact path='/dashboard' render={() => 
               <>
                 <Link to={'/'} className="link">
@@ -20,7 +25,7 @@ const Nav = ({resetFilteredReviews}) => {
             <Route exact path='/' render={() => 
               <>
                 <Link to={'/dashboard'} className="link">
-                    <button><img src={toDo} className='icon'></img></button>
+                    <button><img src={toDo} className='icon' alt='home'></img></button>
                 </Link>
                 <Link to={'/new'} className="link">
                     <button>New Request</button>
@@ -34,7 +39,7 @@ const Nav = ({resetFilteredReviews}) => {
                     <button onClick={() => resetFilteredReviews()}>Home</button>
                 </Link>
                 <Link to={'/dashboard'} className="link">
-                    <button><img src={toDo} className='icon'></img></button>
+                    <button><img src={toDo} className='icon' alt='dashboard'></img></button>
                 </Link>
               </>
             }/>

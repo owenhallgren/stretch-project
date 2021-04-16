59aad685bc00f18125a09be945f15176d0862257
JS <jeff.shep777@gmail.com>
2021-04-15 20:49:05 -0600

    Add dynamic image display based on language

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,8 +2,15 @@ import React from 'react';
 import './Card.css';
 import Modal from '../Modal/Modal'
 
-const Card = ( {username, summary, email, language, date, id, addReview} ) => {
-    return (
+
+
+
+
+const Card = ( {username, summary, language, date, id, addReview} ) => {
+
+  const path = `./assets/${language}.png`
+
+  return (
         <article className='card'>
           <div className='card-top'>
             <img src={`https://avatars.githubusercontent.com/${username}`} className='user-picture' alt='github profile'></img>
@@ -13,7 +20,7 @@ const Card = ( {username, summary, email, language, date, id, addReview} ) => {
             <p>Username: {username}</p>
             <p>Summary: {summary}</p>
             <div className='card-specifics'>
-                <p>{language}</p>
+                <p><img src={path} /> {language}</p>
                 <p>{date}</p>
             </div>
         </article>

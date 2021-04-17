b898b054dec2c55986dc97176d1987019329afea
JS <jeff.shep777@gmail.com>
2021-04-16 20:16:02 -0600

    Add fetch request to deleteReview method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -38,79 +38,84 @@ class App extends Component {
   }
   
 
-sortByLanguage = (language) => {
-  const fReviews = this.state.openReviews.filter(review => review.language === language && !review.reviewer)
-  console.log(language)
-  if(!language) {
-    this.setState({ filteredReviews: [], noFilteredReviews: false, filterValue: language})//fix applied here
-  } else if(fReviews.length){
-    this.setState({ filteredReviews: fReviews, noFilteredReviews: false, filterValue: language})
-  } else if(language !== ''){
-    this.setState({ noFilteredReviews: true, filterValue: language })
-  } else {
-    console.log("and that's a miss")//remove once done testing
+  sortByLanguage = (language) => {
+    const fReviews = this.state.openReviews.filter(review => review.language === language && !review.reviewer)
+    console.log(language)
+    if(!language) {
+      this.setState({ filteredReviews: [], noFilteredReviews: false, filterValue: language})//fix applied here
+    } else if(fReviews.length){
+      this.setState({ filteredReviews: fReviews, noFilteredReviews: false, filterValue: language})
+    } else if(language !== ''){
+      this.setState({ noFilteredReviews: true, filterValue: language })
+    } else {
+      console.log("and that's a miss")//remove once done testing
+    }
   }
-}
 
-finishReview = (e) => {
-  fetch(`http://localhost:3003/api/v1/reviews/complete/${e.target.id}`, {
-    method: 'PUT'
-  })
-    .then((response) => response.json())
-    .then((reviews) => this.setState({ openReviews: reviews }))
-    .catch((error) => console.log(error))
-}
-
-undoReview = (e) => {
-  fetch(`http://localhost:3003/api/v1/reviews/undo/${e.target.id}`, {
-    method: 'PUT'
-  })
-    .then((response) => response.json())
-    .then((reviews) => this.setState({ openReviews: reviews }))
-    .catch((error) => console.log(error))
-}
-
-
-cancelReview = (e) => {
-  fetch(`http://localhost:3003/api/v1/reviews/cancel/${e.target.id}`, {
-    method: 'PUT'
-  })
-    .then((response) => response.json())
-    .then((reviews) => this.setState({ openReviews: reviews }))
-    .catch((error) => console.log(error))
-}
-
-
-submitNewReview = (partialRequest) => {
-  const newRequest = {...partialRequest, username: this.state.username, email: this.state.email, 
-    status: '', reviewer: ''}
-    fetch(`http://localhost:3003/api/v1/reviews`, {
-    method: 'POST',headers: {
-        "Content-Type": "application/json"
-      },
-    body: JSON.stringify({
-      date: newRequest.date,
-      email: newRequest.email,
-      language: newRequest.language, 
-      repo: newRequest.repo,
-      reviewer: newRequest.reviewer,
-      status: newRequest.status,
-      summary: newRequest.summary, 
-      username: newRequest.username
+  finishReview = (e) => {
+    fetch(`http://localhost:3003/api/v1/reviews/complete/${e.target.id}`, {
+      method: 'PUT'
     })
-  })
+      .then((response) => response.json())
+      .then((reviews) => this.setState({ openReviews: reviews }))
+      .catch((error) => console.log(error))
+  }
 
+  undoReview = (e) => {
+    fetch(`http://localhost:3003/api/v1/reviews/undo/${e.target.id}`, {
+      method: 'PUT'
+    })
+      .then((response) => response.json())
+      .then((reviews) => this.setState({ openReviews: reviews }))
+      .catch((error) => console.log(error))
+  }
+
+
+  cancelReview = (e) => {
+    fetch(`http://localhost:3003/api/v1/reviews/cancel/${e.target.id}`, {
+      method: 'PUT'
+    })
+      .then((response) => response.json())
+      .then((reviews) => this.setState({ openReviews: reviews }))
+      .catch((error) => console.log(error))
+  }
+
+
+  submitNewReview = (partialRequest) => {
+    const newRequest = {...partialRequest, username: this.state.username, email: this.state.email, 
+      status: '', reviewer: ''}
+      fetch(`http://localhost:3003/api/v1/reviews`, {
+      method: 'POST',headers: {
+          "Content-Type": "application/json"
+        },
+      body: JSON.stringify({
+        date: newRequest.date,
+        email: newRequest.email,
+        language: newRequest.language, 
+        repo: newRequest.repo,
+        reviewer: newRequest.reviewer,
+        status: newRequest.status,
+        summary: newRequest.summary, 
+        username: newRequest.username
+      })
+    })
+
+      .then((response) => response.json())
+      .then((review) => this.setState({ openReviews:[review[0], ...this.state.openReviews] }))
+      .catch((error) => console.log(error))
+  }
+
+  deleteReview = (e) => {
+    console.log('deleted', e.target.id)//delete once working
+
+    fetch(`http://localhost:3003/api/v1/reviews/delete/${e.target.id}`, {
+      method: 'DELETE'
+    })
     .then((response) => response.json())
-    .then((review) => this.setState({ openReviews:[review[0], ...this.state.openReviews] }))
+    .then((reviews) => this.setState({ openReviews: reviews }))
     .catch((error) => console.log(error))
-}
 
-deleteReview = (e) => {
-console.log('delete me', e.target.id)
-
-
-
-}
+  }
 
 
 

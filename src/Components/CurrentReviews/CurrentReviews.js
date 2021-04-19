9bebb24ab2c70361f91d5284c6632bcb62789d16
Owen Hallgren <owenhallgren@gmail.com>
2021-04-19 13:17:45 -0600

    Cleaned up white space

--- src/Components/CurrentReviews/CurrentReviews.js
+++ src/Components/CurrentReviews/CurrentReviews.js
@@ -6,12 +6,12 @@ const CurrentReviews = ( { state, finishReview, undoReview, cancelReview, delete
   const reviewTable = matchedReviews.map(review => {
     return(
       <tr key={review.id}>
-          <td>{review.date}</td>
-          <td>{review.username}</td>
-          <td><a href={review.repo} target='_blank' rel="noreferrer">{review.repo}</a></td>
-          <td>{review.email}</td>
-          <td><button title="complete and close this review request" className='complete-button dash-button' id={review.id} onClick={(e) => {finishReview(e); sendEmail('completed', e)}}>Complete</button></td>
-          <td><button title="I am unable to complete this review" className='cancel-button dash-button' id={review.id} onClick={(e) => {cancelReview(e); sendEmail('canceled', e)}}>X</button></td>
+        <td>{review.date}</td>
+        <td>{review.username}</td>
+        <td><a href={review.repo} target='_blank' rel="noreferrer">{review.repo}</a></td>
+        <td>{review.email}</td>
+        <td><button title="complete and close this review request" className='complete-button dash-button' id={review.id} onClick={(e) => {finishReview(e); sendEmail('completed', e)}}>Complete</button></td>
+        <td><button title="I am unable to complete this review" className='cancel-button dash-button' id={review.id} onClick={(e) => {cancelReview(e); sendEmail('canceled', e)}}>X</button></td>
       </tr>
     )
   })
@@ -20,11 +20,11 @@ const CurrentReviews = ( { state, finishReview, undoReview, cancelReview, delete
   const completedReviewTable = completedReviews.map(review => {
     return(
       <tr key={review.id}>
-          <td>{review.date}</td>
-          <td>{review.username}</td>
-          <td><a href={review.repo} target='_blank' rel="noreferrer">{review.repo}</a></td>
-          <td>{review.email}</td>
-          <td><button title="return request to my active queue" className='undo-button dash-button' id={review.id} onClick={(e) => undoReview(e)}>Undo</button></td>
+        <td>{review.date}</td>
+        <td>{review.username}</td>
+        <td><a href={review.repo} target='_blank' rel="noreferrer">{review.repo}</a></td>
+        <td>{review.email}</td>
+        <td><button title="return request to my active queue" className='undo-button dash-button' id={review.id} onClick={(e) => undoReview(e)}>Undo</button></td>
       </tr>
     )
   })
@@ -33,17 +33,14 @@ const CurrentReviews = ( { state, finishReview, undoReview, cancelReview, delete
   const completedRequestTable = myReviews.map(review => {
     return(
       <tr key={review.id}>
-          <td>{review.date}</td>
-          <td>{review.username}</td>
-          <td><a href={review.repo} target='_blank' rel="noreferrer">{review.repo}</a></td>
-          <td>{review.status || 'pending'}</td>
-          <td><button title="delete this review request" className="dash-button" id={review.id} onClick={(e) => deleteReview(e)}>Delete</button></td>
-
+        <td>{review.date}</td>
+        <td>{review.username}</td>
+        <td><a href={review.repo} target='_blank' rel="noreferrer">{review.repo}</a></td>
+        <td>{review.status || 'pending'}</td>
+        <td><button title="delete this review request" className="dash-button" id={review.id} onClick={(e) => deleteReview(e)}>Delete</button></td>
       </tr>
     )
   })
-
-
 
     return (
       <>
@@ -51,13 +48,13 @@ const CurrentReviews = ( { state, finishReview, undoReview, cancelReview, delete
         <h1 className='table-header'>My Requests</h1>
         <table>
         <thead>
-            <tr>
-                <th>Date</th>
-                <th>Name</th>
-                <th>Repo</th>
-                <th>Status</th>
-                <th></th>
-        </tr>
+          <tr>
+            <th>Date</th>
+            <th>Name</th>
+            <th>Repo</th>
+            <th>Status</th>
+            <th></th>
+          </tr>
         </thead>
         <tbody>
            {!completedRequestTable.length && <tr className="empty-review-table"><td>no reviews available</td></tr>} 
@@ -65,22 +62,18 @@ const CurrentReviews = ( { state, finishReview, undoReview, cancelReview, delete
         </tbody>
       </table>
       </div>
-
-
-
-
       <div className='table-container'>
         <h1 className='table-header'>Active Reviews</h1>
         <table id='active'>
         <thead>
-            <tr>
-                <th>Date</th>
-                <th>Name</th>
-                <th>Repo</th>
-                <th>Email</th>
-                <th></th>
-                <th></th>
-        </tr>
+          <tr>
+            <th>Date</th>
+            <th>Name</th>
+            <th>Repo</th>
+            <th>Email</th>
+            <th></th>
+            <th></th>
+          </tr>
         </thead>
         <tbody>
            {!reviewTable.length && <tr className="empty-review-table"><td>no reviews available</td></tr>}
@@ -89,22 +82,22 @@ const CurrentReviews = ( { state, finishReview, undoReview, cancelReview, delete
       </table>
       </div>
       <div className='table-container'>
-      <h1 className='table-header'>Completed Reviews</h1>
-        <table id='completed'>
-        <thead>
+        <h1 className='table-header'>Completed Reviews</h1>
+          <table id='completed'>
+          <thead>
             <tr>
-                <th>Date</th>
-                <th>Name</th>
-                <th>Repo</th>
-                <th>Email</th>
-                <th></th>
-        </tr>
-        </thead>
-        <tbody>
-          {!completedReviewTable.length && <tr className="empty-review-table"><td>no reviews available</td></tr>}
-          {completedReviewTable}
-        </tbody>
-      </table>
+              <th>Date</th>
+              <th>Name</th>
+              <th>Repo</th>
+              <th>Email</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {!completedReviewTable.length && <tr className="empty-review-table"><td>no reviews available</td></tr>}
+            {completedReviewTable}
+          </tbody>
+        </table>
       </div>
       </>
     )
